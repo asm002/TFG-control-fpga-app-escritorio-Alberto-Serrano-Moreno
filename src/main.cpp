9ff25d0a1222ab60d679d888ad4e9ab3158be6ee
asm002 <asm00152@red.ujaen.es>
2025-12-26 00:09:34 +0100

    menu bienvenida y menu principal version minima

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #include <FL/Fl_Box.h>
 #include <FL/Fl_Wizard.H>
 
+void boton_callback(Fl_Widget *w, void *data) {
+    Fl_Wizard* wizard = static_cast<Fl_Wizard*>(data);
+    wizard->next();
+}
+
 int main()
 {
     Fl_Window ventana(0, 0, 600, 338, "Control de temperatura - Alberto Serrano Moreno");
@@ -17,11 +22,32 @@ int main()
     Fl_Box tituloBienvenida(0, 0, ventana.w(), 40, "Bienvenido");
     tituloBienvenida.labelsize(24);
 
+    const int ancho = 200;
+    Fl_Button boton{ventana.w()/2-ancho/2, ventana.h()/2, ancho, 20, "Menú Principal"};
+    boton.callback(boton_callback, &wizard);
+
     grupoBienvenida.end();
 
+
+    /* =======================
+       PANTALLA(GRUPO) PRINCIPAL
+       ======================= */
+    Fl_Group grupoPrincipal(0, 0, ventana.w(), ventana.h());
+
+    Fl_Box titulo(0, 0, ventana.w(), 40, "Control de temperatura STM32");
+    titulo.labelsize(24);
+
+    grupoPrincipal.end();
+
+
+
     wizard.end();
-    wizard.value(&grupoBienvenida); // Página inicial
+    ventana.end();
+
+    //wizard.value(&grupoPrincipal); // Página inicial
 
     ventana.show();
     return Fl::run();
 }
+
+
