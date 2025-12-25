94e400126f4db550b8bfad47718a0fb215e42763
asm002 <asm00152@red.ujaen.es>
2025-12-25 22:07:00 +0100

    Primer commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+#include <FL/Fl.h>
+#include <FL/Fl_Window.h>
+#include <FL/Fl_Button.h>
+#include <FL/Fl_Box.h>
+#include <FL/Fl_Wizard.H>
+
 int main()
 {
+    Fl_Window ventana(0, 0, 600, 338, "Control de temperatura - Alberto Serrano Moreno");
+    Fl_Wizard wizard{0, 0, ventana.w(), ventana.h()};
+
+    /* =======================
+       PANTALLA(GRUPO) BIENVENIDA
+       ======================= */
+    Fl_Group grupoBienvenida(0, 0, ventana.w(), ventana.h());
+
+    Fl_Box tituloBienvenida(0, 0, ventana.w(), 40, "Bienvenido");
+    tituloBienvenida.labelsize(24);
+
+    grupoBienvenida.end();
+
+    wizard.end();
+    wizard.value(&grupoBienvenida); // Página inicial
+
+    ventana.show();
+    return Fl::run();
 }
