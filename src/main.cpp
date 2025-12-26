24a5767578d544631191bd0aacc6f94189eba953
asm002 <asm00152@red.ujaen.es>
2025-12-26 01:24:51 +0100

    ventana de bienvenida con desplegable de puertos serie

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,41 @@
 #include <FL/Fl_Button.h>
 #include <FL/Fl_Box.h>
 #include <FL/Fl_Wizard.H>
+#include <FL/Fl_Choice.H>
+
+#include <windows.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 void boton_callback(Fl_Widget *w, void *data) {
     Fl_Wizard* wizard = static_cast<Fl_Wizard*>(data);
     wizard->next();
+}
+
+std::vector<std::string> buscar_puertos_serie() {
+    std::vector<std::string> puertos;
+
+    for (int i = 1; i <= 256; i++) {
+        std::string nombre = "\\\\.\\COM" + std::to_string(i);
+
+        HANDLE h = CreateFileA(
+            nombre.c_str(),
+            GENERIC_READ | GENERIC_WRITE,
+            0,
+            nullptr,
+            OPEN_EXISTING,
+            0,
+            nullptr
+        );
+
+        if (h != INVALID_HANDLE_VALUE) {
+            puertos.push_back("COM" + std::to_string(i));
+            CloseHandle(h);
+        }
+    }
+    return puertos;
 }
 
 int main()
@@ -22,9 +53,22 @@ int main()
     Fl_Box tituloBienvenida(0, 0, ventana.w(), 40, "Bienvenido");
     tituloBienvenida.labelsize(24);
 
-    const int ancho = 200;
-    Fl_Button boton{ventana.w()/2-ancho/2, ventana.h()/2, ancho, 20, "Menú Principal"};
+    const int anchoBoton = 200;
+    const int altoBoton = 40;
+    const int xBoton = ventana.w()/2-anchoBoton/2;
+    const int yBoton = ventana.h()/2;
+    Fl_Button boton{xBoton, yBoton, anchoBoton, altoBoton, "Conectar"};
     boton.callback(boton_callback, &wizard);
+
+    const int anchoDesplegable = 100;
+    const int altoDesplegable = 25;
+    const int xDesplegable = xBoton + anchoDesplegable/2;
+    const int yDesplegable = yBoton + (altoBoton/2) + altoDesplegable;
+    Fl_Choice desplegableCOM{xDesplegable, yDesplegable, anchoDesplegable, altoDesplegable, "Puerto serie:"};
+    auto puertos = buscar_puertos_serie();
+    for (auto& p : puertos) {
+        desplegableCOM.add(p.c_str());
+    }
 
     grupoBienvenida.end();
 
